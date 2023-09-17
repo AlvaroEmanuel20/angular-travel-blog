9423469301b9998c0485659c26b4a3edb36ddaa4
Álvaro Emanuel <alvarodiasribeiro16@gmail.com>
2023-09-17 10:05:23 -0300

    feat: 404 not found page design

--- src/app/pages/page-not-found/page-not-found.component.ts
+++ src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,8 +1,20 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.css'],
 })
-export class PageNotFoundComponent {}
+export class PageNotFoundComponent {
+  constructor(private location: Location, private navigator: Router) {}
+
+  goBackHome() {
+    this.navigator.navigate(['/']);
+  }
+
+  goBack() {
+    this.location.back();
+  }
+}
